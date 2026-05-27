20df7b4fb246b1d202d0ed6260c16a5001b23974
Mussussa <xismaelchaibo@gmail.com>
2026-05-27 23:26:31 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'oxc', // 🎯 Voltamos para o padrão ultra-rápido do Vite 8
+    chunkSizeWarningLimit: 3000, // 🚀 Aumenta o limite para 3MB para aceitar as tuas imagens
+  }
 })
